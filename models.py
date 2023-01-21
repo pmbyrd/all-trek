22076f52220a1807f7e272d955fc5727cf25740e
Perriesha <pmbyrd@gmail.com>
2023-01-20 20:39:25 -0800

    setting up env

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-
+"""This module contains the models for the database, user, post, tag, post_tag."""
 # *Importing the required modules
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,3 +9,9 @@ def connect_db(app):
     """Connection to the database that is called in app.py for the Flask app to use."""
     db.app = app
     db.init_app(app)
+    
+class User():
+    """Creates a user model for the database."""
+    __tablename__ = "users"
+    
+    
---
+++ models_star_trek.py
@@ -0,0 +1,3 @@
+"""This module contains the Star Trek models for the database."""
+
+from models import db   
