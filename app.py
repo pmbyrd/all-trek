3b6bf92712115ae9d5814cc84164b4c1742e6d8c
Perriesha <pmbyrd@gmail.com>
2023-01-21 23:10:53 -0800

    api working

--- app.py
+++ app.py
@@ -5,7 +5,9 @@
 # !
 from flask import Flask, render_template, request, redirect, url_for, flash, sessions, g, jsonify
 import os
-from config import api_key 
+import json
+
+
 
 
 app = Flask(__name__)
@@ -16,8 +18,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #!don't know if this is necessary
 app.config['API_KEY'] = os.environ.get('API_KEY', "/config.json") #!don't know if this is configured correctly
+app.config.from_file("config.json", load=json.load)
 
 
+@app.route('/api_key')
+def api_key():
+    """Gets the api key from config.json"""
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    api_key = config['API_KEY']
+    
+    return jsonify(api_key=api_key)
 
 @app.route('/')
 def root():
--- static/js/trek.js
+++ static/js/trek.js
@@ -1,1 +1,40 @@
-console.log('Hello World');
+
+// const apiKey = await getApiKey() //?Why is this code working?
+// console.log('Hello World');
+// // get the json response of an api_key from my server
+// async function getApiKey(){
+//     const res = await axios.get('/api_key')
+//     console.table(res.data)
+//     return res.data.api_key
+// }
+const Base_URL = 'https://api.themoviedb.org/3'
+const 
+
+async function getApiKey(){
+    const res = await axios.get('/api_key')
+    console.table(res.data)
+    // const apiKey = JSON.parse(res.data.api_key)
+    const data = res.data.api_key
+    return data
+}
+
+// test the api_key
+// todo make a function call to tmdb
+async function getMovie(movie){
+    const apiKey = await getApiKey()
+    const res = await axios.get(`${Base_URL}/search/movie/?api_key=${apiKey}&query=${movie}`)
+    // console.log(res.data)
+    return res.data
+}
+
+// todo make a function to display the movies
+async function displayMovies(){
+    const res = await getMovie("star trek")
+    const results = res.results
+    console.table(results)
+    const 
+    return results
+    
+}
+
+
